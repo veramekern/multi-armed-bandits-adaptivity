994d594dcf456abc71f32017aa288f2c1047674c
Vera Mekern <veertje.m@gmail.com>
2020-06-27 00:08:23 +0200

    added some descriptive analyses, checked participants' choice distributions vs. optimal distributions.

--- scripts/chapter-3-study-1-statistical-analysis.R
+++ scripts/chapter-3-study-1-statistical-analysis.R
@@ -12,7 +12,7 @@ ipak <- function(pkg){
     install.packages(new.pkg, dependencies = T)
   sapply(pkg, require, character.only = T)
 }
-packages <- c("dplyr", "plotly", "ggplot2", "gridExtra", "tidyr")
+packages <- c("dplyr", "plotly", "ggplot2", "gridExtra", "tidyr", "matrixStats")
 ipak(packages)
 
 # ---------- LOAD DATA ---------- 
@@ -188,7 +188,113 @@ perf.arm.violin <-  dat.collect %>%
 
 grid.arrange(switch.violin, clust.violin, perf.violin, perf.arm.violin, ncol=2)
 
+# ---------- DESCRIPTIVES ---------- 
+# calculate how many times arm was best arm per version
+
+table.best <- dat %>%
+  group_by(version, trial) %>%
+  summarise(best = mean(best.possible))
+split.table.best <- split(x=table.best, f=table.best$version)
+
+best.a <- c(79, 0, 70, 51)
+best.b <- c(47, 120, 25, 8)
+
+# show distribution choice pp
+dat.guess.1 <- dat %>%
+  group_by(id) %>%
+  filter(choice==1) %>%
+  tally(choice)
+
+dat.guess.2 <- dat %>%
+  group_by(id) %>%
+  filter(choice==2) %>%
+  tally(choice)
+dat.guess.2$n <- dat.guess.2$n/2
+
+dat.guess <- full_join(dat.guess.1, dat.guess.2, by="id")
+
+dat.guess.3 <- dat %>%
+  group_by(id) %>%
+  filter(choice==3) %>%
+  tally(choice)
+dat.guess.3$n <- dat.guess.3$n/3
+
+dat.guess <- full_join(dat.guess, dat.guess.3, by="id")
+
+dat.guess.4 <- dat %>%
+  group_by(id) %>%
+  filter(choice==4) %>%
+  tally(choice)
+dat.guess.4$n <- dat.guess.4$n/4
+
+dat.guess.observed <- full_join(dat.guess, dat.guess.4, by="id")
+names(dat.guess.observed) <- c("id", "n.1", "n.2", "n.3", "n.4")
+version <- select(dat.collect, id, version)
+dat.guess <- full_join(version, dat.guess.observed, by="id")
+
+# add expected distribution pp
+dat.guess.expected <- dat.guess %>%
+  summarise(case_when(version=="a" ~ best.a, 
+                      version=="b" ~ best.b)) 
+names(dat.guess.expected) <- c("id", "expected.choice")
+arm <- rep(c(1,2,3,4), 64)
+dat.guess.expected <- cbind(dat.guess.expected, arm)
+names(dat.guess.expected)[3] <- "arm"
+dat.guess.expected <- spread(dat.guess.expected, arm, expected.choice)
+names(dat.guess.expected) <- c("id", "e.1", "e.2", "e.3", "e.4")
+
+head(dat.guess.expected)
+head(dat.guess.observed)
+dat.guess <- full_join(dat.guess.observed, dat.guess.expected, by="id")
+head(dat.guess)
+
+chisq <- dat.guess %>%
+  rowwise() %>% 
+  mutate(
+    test_stat = chisq.test(c(n.1:n.4, e.1:e.4))$statistic,
+    p_val = chisq.test(c(n.1:n.4, e.1:e.4))$p.value
+  )
+View(chisq)
+
+
+chisq %>%
+  ggplot( aes(x=p_val)) +
+  geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
+
+dat.collect <- full_join(dat.collect, chisq, by="id")
+
 # ---------- ANALYSES ---------- 
+
 
 mod1 <- lm(p.correct.arm ~ n.switches, data=dat.collect)
 cooksd <- cooks.distance(mod1)
+
+sample_size <- nrow(dat.collect)
+plot(cooksd, pch="*", cex=2, main="Influential Obs by Cooks distance")
+abline(h = 4/sample_size, col="red") 
+text(x=1:length(cooksd)+1, y=cooksd, labels=ifelse(cooksd>4/sample_size, 
+                                                   names(cooksd),""), col="red")
+
+names(dat.collect)
+
+dat.collect %>%
+  ggplot(aes(x=p_val, y=p.correct.arm)) +
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) +
+  facet_wrap(~ version)
+
+dat.collect %>%
+  ggplot( aes(x=p_val)) +
+  geom_histogram(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+  facet_wrap(~ version)
+
+
+# there is a huge difference between the two versions
+# the relationship between some variables are much more logical in version a
+# this might be due to the fact that there is a much more uniform distribution
+# of which arm is the best choice, even though one arm is never the best choice
+# in version b the best arm is arm 2 most of the time, over half of the trials
+# apparently, though, people have a tendency to keep switching machines as the
+# chisq values indicate that the distribution of their choices does not match 
+# the disrtibution of the best arm, at all. 
+# especially compared to version a
